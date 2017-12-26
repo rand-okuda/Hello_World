af48e28151ba86a15d695b876acdb0d4c65810e9
okuda <okuda@RD12PC061N>
2017-12-26 11:06:12 +0900

    コンソール出力順再々変更

--- src/Hello_World/Main.java
+++ src/Hello_World/Main.java
@@ -13,8 +13,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    System.out.println("Hello, World");
         System.out.println(args);
+        System.out.println("Hello, World");
 
 	}
 
