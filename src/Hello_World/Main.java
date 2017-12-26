fbe057462f48065a07768bec69c83d9231f0d6f4
okuda <okuda@RD12PC061N>
2017-12-26 16:09:46 +0900

    コンソール出力再々追加

--- src/Hello_World/Main.java
+++ src/Hello_World/Main.java
@@ -14,9 +14,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
         System.out.println("---");
-        System.out.println("*");
+        System.out.println("+");
         System.out.println(args +" Hello, World");
-        System.out.println("*");
+        System.out.println("+");
         System.out.println("---");
 
 	}
