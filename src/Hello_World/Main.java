7b2d12fac833e198a9dc378e5d592177465d5c65
okuda <okuda@RD12PC061N>
2017-12-26 16:49:19 +0900

    NOW表示機能追加

--- src/Hello_World/Main.java
+++ src/Hello_World/Main.java
@@ -3,6 +3,9 @@
  */
 package Hello_World;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * @author okuda
  *
@@ -18,7 +21,20 @@ public class Main {
         System.out.println(args +" Hello, World");
         System.out.println("*");
         System.out.println("---");
+        String wt = WhatsTime();
+        System.out.println(wt);
 
 	}
+
+	/**
+     * @return returnString
+     */
+    public static String WhatsTime() {
+        String returnString;
+        Calendar returnDate = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
+        returnString =sdf.format(returnDate.getTime());
+        return returnString;
+    }
 
 }
