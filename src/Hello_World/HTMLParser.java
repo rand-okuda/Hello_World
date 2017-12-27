6be5441b902bd65f3c945ca308b5d16878706472
okuda <okuda@RD12PC061N>
2017-12-27 16:30:49 +0900

    Array作機能追加

--- src/Hello_World/HTMLParser.java
+++ src/Hello_World/HTMLParser.java
@@ -3,6 +3,8 @@
  */
 package Hello_World;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -48,18 +50,50 @@ public class HTMLParser {
         return tag;
     }
 
+    /**
+     * main()
+     *引数が空白の場合はメッセージをコンソール出力
+     *引数が与えられた場合はHTMLパーサーで処理
+     * @param args
+     */
     public static void main(String[] args) {
+        // 引数が与えられた場合
         if (args.length > 0) {
-            // パーサオブジェクトを作成
-            HTMLParser parser = new HTMLParser(args[0]);
-            //タグを順番に処理
-            while (parser.hasNext()) {
-                Tag tag = parser.nextTag();
+            // HTMLタグのパーサに引数を代入しListを作成
+            List<Tag> tagList = TagArray(args[0]);
+            // Listを準備処理
+            for (Tag tag : tagList) {
+                // HTMLタグをコンソール出力し動作確認
                 System.out.println(tag);
             }
+        // 引数が与えられなかった場合
         }else {
+            // メッセージをコンソール出力
             System.out.println("引数にHTML文字列をワンワード（ブランクなし）で代入してください。");
         }
+
+    }
+
+    /**
+     * 次のHTMLタグObjectの配列を返却
+     * @param htmlString
+     * @return tagArray
+     */
+    public static List<Tag> TagArray(String htmlString) {
+        // 返却用Listを作成
+        List<Tag> tagArray = new ArrayList<>();
+        // パーサオブジェクトを作成
+        HTMLParser parser = new HTMLParser(htmlString);
+        //次のタグが存在する場合Listを順番に処理
+        while (parser.hasNext()) {
+            // タグを作成
+            Tag tag = parser.nextTag();
+            // タグを返却用Listに追加
+            tagArray.add(tag);
+        }
+        // 返却用リストをReturn
+        return tagArray;
+
     }
 
 }
